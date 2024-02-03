ebd5e1d5d024f9ca68840faa4f383a892a959cec
Ahmed mohamed taha <ahmed.wesal03@eng-st.cu.edu.eg>
2024-02-03 17:19:55 +0200

    finishing circular max subarray

--- qn3.cpp
+++ qn3.cpp
@@ -3,20 +3,37 @@ using namespace std;
 #include <vector>
 
 
+//918. Maximum Sum Circular Subarray
+//
+//Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
+//A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n]
+//and the previous element of nums[i] is nums[(i - 1 + n) % n].
+//A subarray may only include each element of the fixed buffer nums at most once. Formally, for a subarray nums[i], nums[i + 1], ..., nums[j],
+//there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
+
 class Solution {
 public:
-    int maxSubarraySumCircular(vector<int>& nums) {
-        auto maxsum = nums[0];
-        int current_sum = 0;
-        for (n: nums)
+    int maxSubarraySumCircular(vector<int>& nums)
+    {
+        // first of all I need the max and min to get the cir
+        auto curmax = nums[0];
+        auto curmin = nums[0];
+        auto globmax = 0;
+        auto globmin = 0;
+        int total = 0;
+        for (int n : nums)
         {
-            current_sum = max(current_sum, 0);
-            current_sum+= n;
-            maxsum = max(current_sum, maxsum);
+            curmax = max(curmax + n, n);
+            curmin = min(curmin +n, n);
+            total +=n;
+            globmax = max(curmax, globmax);
+            globmin = min(curmin, globmin);
         }
-
-
-
+        if (globmax >0)
+            return max(globmax, globmin);
+        return globmax;
 
     }
 };
+
+
