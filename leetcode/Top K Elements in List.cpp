e0c1a5b4792faba15a494095241f0cd010ec787e
Ahmed mohamed taha <ahmed.wesal03@eng-st.cu.edu.eg>
2024-06-19 05:03:36 +0300

    topKFrequent

---
+++ leetcode/Top K Elements in List.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+#include <unordered_map>
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> frequencyMap;
+        vector<int> result;
+        result.reserve(k);
+
+        // Count frequencies
+        for (int num : nums) {
+            frequencyMap[num]++;
+        }
+
+        // Find the top K frequent elements
+        for (int i = 0; i < k; i++) {
+            int currentMaxFreq = 0;
+            int maxElement = 0;
+            for (auto& pair : frequencyMap) {
+                if (pair.second > currentMaxFreq) {
+                    currentMaxFreq = pair.second;
+                    maxElement = pair.first;
+                }
+            }
+            result.push_back(maxElement);
+            frequencyMap.erase(maxElement);
+        }
+
+        return result;
+    }
+};
